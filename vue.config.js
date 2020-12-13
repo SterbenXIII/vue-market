d994ffc811df889e2613511e5c9c3ac1d5cc628b
SterbenXIII <sterbenxiiosu@gmail.com>
2020-12-13 21:23:30 +0200

    new file:   db.json 	new file:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	modified:   src/App.vue 	new file:   src/assets/images/1.jpg 	new file:   src/assets/images/2.jpg 	new file:   src/assets/images/3.jpg 	new file:   src/assets/images/4.jpg 	new file:   src/assets/images/5.jpg 	new file:   src/assets/images/6.jpeg 	new file:   src/assets/styles/styles.scss 	new file:   src/assets/styles/variables.scss 	deleted:    src/components/HelloWorld.vue 	new file:   src/components/cart/v-cart-item.vue 	new file:   src/components/cart/v-cart.vue 	new file:   src/components/catalog/v-catalog-item.vue 	new file:   src/components/catalog/v-catalog.vue 	new file:   src/components/catalog/v-product-page.vue 	new file:   src/components/catalog/v-textfield.vue 	new file:   src/components/layouts/v-header.vue 	new file:   src/components/main-page/v-main-page.vue 	new file:   src/components/notifications/v-notification.vue 	new file:   src/components/popup/v-popup.vue 	new file:   src/components/v-card.vue 	new file:   src/components/v-main-wrapper.vue 	new file:   src/components/v-select.vue 	new file:   src/filters/price-format.js 	new file:   src/filters/toFix.js 	modified:   src/main.js 	new file:   src/router/router.js 	new file:   src/vuex/actions/actions.js 	new file:   src/vuex/actions/api-requests.js 	new file:   src/vuex/getters/getters.js 	new file:   src/vuex/mutations/mutations.js 	new file:   src/vuex/store.js 	new file:   vue.config.js 	new file:   webpack.config.js 	modified:   yarn.lock

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+  css: {
+
+      loaderOptions: {
+
+          sass: {
+
+            prependData: `@import "@/assets/styles/variables.scss";`
+
+          },
+
+      },
+
+  }
+
+};
